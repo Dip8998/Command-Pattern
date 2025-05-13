9bffd3bb4b98785835219a15261f8e6ae40f1f36
Dip Patel <pateldeep2332@gmail.com>
2025-05-13 23:44:13 +0530

    Created Concrete Commands

---
+++ Assets/Scripts/Commands/ConcreteCommand/AttackStance.cs
@@ -0,0 +1,18 @@
+﻿using Command.Actions;
+using Command.Main;
+using Command.Player;
+
+public class AttackStance : UnitCommand
+{
+    private bool willHitTarget;
+
+    public AttackStance(CommandData commandData)
+    {
+        this.commandData = commandData;
+        willHitTarget = WillHitTarget();
+    }
+
+    public override bool WillHitTarget() => true;
+
+    public override void Execute() => GameService.Instance.ActionService.GetActionByType(CommandType.AttackStance).PerformAction(actorUnit, targetUnit, willHitTarget);
+}
